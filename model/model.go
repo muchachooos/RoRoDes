27b48dc4bff5463add6128a2703f0d03204758b0
muchachooos <odnorogg@bk.ru>
2023-01-15 23:08:26 +0400

    Update model.go

--- model/model.go
+++ model/model.go
@@ -1,1 +1,23 @@
 package model
+
+type Config struct {
+	DataSourceName string `json:"dataSourceName"`
+	Port           int    `json:"port"`
+	Key            string `json:"auth_key"`
+}
+
+type GameField struct {
+	Field int     `json:"field"`
+	Unit  *string `json:"unit"`
+}
+
+type Game struct {
+	Fields []GameField `json:"game"`
+}
+
+//type T struct {
+//	Game []struct {
+//		Field string  `json:"field"`
+//		Unit  *string `json:"unit"`
+//	} `json:"game"`
+//}
