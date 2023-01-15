106ce3d2d9104ba8a01a1a6a4d2c0b72f74c8d40
muchachooos <odnorogg@bk.ru>
2023-01-15 23:16:05 +0400

    Create configuration.go

---
+++ configuration/configuration.go
@@ -0,0 +1,23 @@
+package configuration
+
+import (
+	"GameAPI/model"
+	"encoding/json"
+	"os"
+)
+
+func GetConfig(path string) model.Config {
+	configInBytes, err := os.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+
+	var config model.Config
+
+	err = json.Unmarshal(configInBytes, &config)
+	if err != nil {
+		panic(err)
+	}
+
+	return config
+}
