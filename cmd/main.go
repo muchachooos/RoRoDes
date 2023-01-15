8f09c9f848ad1e33913dae9e9b85d52ed86734f2
muchachooos <odnorogg@bk.ru>
2023-01-15 23:07:50 +0400

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
+	"GameAPI/model"
+	"encoding/json"
 	"github.com/gin-gonic/gin"
+	"os"
+	"strconv"
 )
 
 func main() {
-	router := gin.Default()
+	configInBytes, err := os.ReadFile("./configuration.json")
+	if err != nil {
+		panic(err)
+	}
+
+	var config model.Config
+
+	err = json.Unmarshal(configInBytes, &config)
+	if err != nil {
+		panic(err)
+	}
+
+	engine := gin.Default()
+
+	engine.POST("/some_path", func(context *gin.Context) {})
+
+	err = engine.Run(":" + strconv.Itoa(config.Port))
+	if err != nil {
+		panic(err)
+	}
 }
