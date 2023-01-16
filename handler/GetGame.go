3bae4045f0b4cdb847c84880ee9fbb00d2172e8c
muchachooos <odnorogg@bk.ru>
2023-01-16 23:44:10 +0400

    Update GetGame.go

--- handler/GetGame.go
+++ handler/GetGame.go
@@ -3,7 +3,6 @@ package handler
 import (
 	"GameAPI/model"
 	"encoding/json"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"io"
@@ -45,16 +44,42 @@ func (s *DataBase) CreateGame(context *gin.Context) {
 
 	id := uuid.NewString()
 
-	_, err = s.DB.Exec("INSERT INTO game (`id`) VALUES (?)", id)
+	query := `INSERT INTO game (id, field_1, field_2, field_3, field_4, field_5, field_6, field_7, field_8, field_9, field_10,
+                  field_11, field_12, field_13, field_14, field_15, field_16, field_17, field_18, field_19, field_20,
+                  field_21, field_22, field_23, field_24, field_25, field_26, field_27, field_28, field_29, field_30,
+                  field_31, field_32, field_33, field_34, field_35, field_36, field_37, field_38, field_39, field_40)
+VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
+        ?, ?, ?);`
+
+	_, err = s.DB.Exec(query, id)
 	if err != nil {
 		panic(err)
 	}
 
-	for i := range game {
-		query := fmt.Sprintf("UPDATE game SET `field_%d` = ? WHERE `id` = ?", game[i].Number)
-		_, err = s.DB.Exec(query, game[i].Unit, id)
-		if err != nil {
-			panic(err)
+}
+
+/*
+
+
+
+
+
+
+
+
+
+
+_, err = s.DB.Exec("INSERT INTO game (`id`) VALUES (?)", id)
+	if err != nil {
+		panic(err)
+	}
+for i := range game {
+		if game[i].Unit != nil {
+			query := fmt.Sprintf("UPDATE game SET `field_%d` = ? WHERE `id` = ?", game[i].Number)
+			_, err = s.DB.Exec(query, game[i].Unit, id)
+			if err != nil {
+				panic(err)
+			}
 		}
 	}
-}
+*/
