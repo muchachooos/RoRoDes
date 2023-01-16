3780088228f77129f9c458c12a09b5783feb7797
muchachooos <odnorogg@bk.ru>
2023-01-16 19:48:37 +0400

    Update model.go

--- model/model.go
+++ model/model.go
@@ -1,19 +1,23 @@
 package model
 
+type Err struct {
+	Error string `json:"error"`
+}
+
 type Config struct {
 	DataSourceName string `json:"dataSourceName"`
 	Port           int    `json:"port"`
 	Key            string `json:"auth_key"`
 }
 
-type GameField struct {
-	Field int     `json:"field"`
-	Unit  *string `json:"unit"`
+type Field struct {
+	Number int     `json:"number"`
+	Unit   *string `json:"unit"`
 }
 
-type Game struct {
-	Fields []GameField `json:"game"`
-}
+//type Game struct {
+//	Fields []GameField `json:"game"`
+//}
 
 //type T struct {
 //	Game []struct {
