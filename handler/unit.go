a3621e0dc3493605fabf96d1d2a38c5ebc8ce56a
muchachooos <odnorogg@bk.ru>
2023-01-20 01:35:56 +0400

    Update unit.go

--- handler/unit.go
+++ handler/unit.go
@@ -1,7 +1,27 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"GameAPI/model"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
-func (d *DataBase) CreateUnitHandler(context *gin.Context) {
+func (s *Server) CreateUnitHandler(context *gin.Context) {
+	cardID, ok := context.GetQuery("card_id")
+	if cardID == "" || !ok {
+		context.JSON(http.StatusBadRequest, model.Err{Error: "Card ID is missing"})
+		return
+	}
 
+	unit, err := s.Storage.CreateUnitInDB(cardID)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, model.Err{Error: "Database error: " + err.Error()})
+		return
+	}
+
+	if len(unit) == 0 {
+		context.JSON(http.StatusNotFound, model.Err{Error: "No card with this ID: " + err.Error()})
+	}
+
+	context.JSON(http.StatusOK, unit)
 }
