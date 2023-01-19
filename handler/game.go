6b7139454f92f6b9f3aedde151e643777c3137db
muchachooos <odnorogg@bk.ru>
2023-01-19 15:05:19 +0400

    Update game.go

--- handler/game.go
+++ handler/game.go
@@ -2,53 +2,20 @@ package handler
 
 import (
 	"GameAPI/model"
-	"encoding/json"
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
-	"io"
 	"net/http"
 )
 
-func (s *DataBase) InitGameHandler(context *gin.Context) {
-	bodyInBytes, err := io.ReadAll(context.Request.Body)
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, model.Err{Error: "Read body error: " + err.Error()})
-		return
-	}
-
-	err = context.Request.Body.Close()
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, model.Err{Error: "Close body error: " + err.Error()})
-		return
-	}
-
-	var game []model.Field
-
-	err = json.Unmarshal(bodyInBytes, &game)
-	if err != nil {
-		if err != nil {
-			context.JSON(http.StatusBadRequest, model.Err{Error: "Unmarshal request body error: " + err.Error()})
-		}
-	}
-
-	gameId := uuid.NewString()
-
-	_, err = s.DB.Exec("INSERT INTO game (`id`) VALUES (?)", gameId)
+func (d *Server) InitGameHandler(context *gin.Context) {
+	gameID, err := d.Storage.InitGameInDB()
 	if err != nil {
 		panic(err)
 	}
 
-	for i := range game {
-		_, err = s.DB.Exec("INSERT INTO fields (`number`, `unit_id`, `game_id`) VALUES (?,?,?)", game[i].Number, game[i].UnitID, gameId)
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	context.JSON(http.StatusOK, gameId)
+	context.JSON(http.StatusOK, gameID)
 }
 
-func (s *DataBase) GetGameHandler(context *gin.Context) {
+func (d *Server) GetGameHandler(context *gin.Context) {
 	gameID, ok := context.GetQuery("id")
 	if gameID == "" || !ok {
 		context.Writer.WriteString("Game ID is missing")
@@ -57,7 +24,7 @@ func (s *DataBase) GetGameHandler(context *gin.Context) {
 
 	var game []model.Field
 
-	err := s.DB.Select(&game, "SELECT `number`,`unit_id` FROM fields WHERE `game_id` = ?", gameID)
+	err := d.Storage.DB.Select(&game, "SELECT `number`,`unit_id` FROM fields WHERE `game_id` = ?", gameID)
 	if err != nil {
 		panic(err)
 	}
