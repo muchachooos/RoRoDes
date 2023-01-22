1d51e12d26fc4b3c5cad7577a27a5bd55c53a99f
muchachooos <odnorogg@bk.ru>
2023-01-22 15:34:47 +0400

    Update model.go

--- model/model.go
+++ model/model.go
@@ -11,25 +11,27 @@ type Config struct {
 }
 
 type Card struct {
-	ID      string  `db:"id"`
+	CardID  string  `db:"card_id"`
 	Name    *string `db:"name"`
 	Damage  *int    `db:"damage"`
 	Speed   *int    `db:"speed"`
 	Health  int     `db:"health"`
-	Picture byte    `db:"picture"`
+	Picture []byte  `db:"picture"`
 }
 
 type Unit struct {
-	ID     string  `json:"id" db:"id"`
-	CardID string  `json:"card_id" db:"card_id"`
-	Name   *string `json:"name" db:"name"`
-	Damage *int    `json:"damage" db:"damage"`
-	Speed  *int    `json:"speed" db:"speed"`
-	Health int     `json:"health" db:"health"`
+	UnitID   string  `json:"unit_id" db:"unit_id"`
+	CardID   string  `json:"card_id" db:"card_id"`
+	GameID   string  `json:"game_id" db:"game_id"`
+	FieldNum int     `json:"field_num" db:"field_num"`
+	Name     *string `json:"name" db:"name"`
+	Damage   *int    `json:"damage" db:"damage"`
+	Speed    *int    `json:"speed" db:"speed"`
+	Health   int     `json:"health" db:"health"`
 }
 
 type Field struct {
-	Number int     `json:"number" db:"number"`
+	Number int     `json:"field_num" db:"number"`
 	UnitID *string `json:"unit"   db:"unit_id"`
 }
 
