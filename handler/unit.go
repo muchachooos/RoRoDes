21343754268374a238e980bdaf0dfa26dad22d72
muchachooos <odnorogg@bk.ru>
2023-01-22 15:34:44 +0400

    Update unit.go

--- handler/unit.go
+++ handler/unit.go
@@ -4,6 +4,7 @@ import (
 	"GameAPI/model"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 func (s *Server) CreateUnitHandler(context *gin.Context) {
@@ -13,7 +14,21 @@ func (s *Server) CreateUnitHandler(context *gin.Context) {
 		return
 	}
 
-	unit, err := s.Storage.CreateUnitInDB(cardID)
+	gameID, ok := context.GetQuery("game_id")
+	if gameID == "" || !ok {
+		context.JSON(http.StatusBadRequest, model.Err{Error: "Game ID is missing"})
+		return
+	}
+
+	fieldNumInString, ok := context.GetQuery("field_num")
+	if fieldNumInString == "" || !ok {
+		context.JSON(http.StatusBadRequest, model.Err{Error: "Field number is missing"})
+		return
+	}
+
+	fieldNum, err := strconv.Atoi(fieldNumInString)
+
+	unit, err := s.Storage.CreateUnitInDB(cardID, gameID, fieldNum)
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, model.Err{Error: "Database error: " + err.Error()})
 		return
