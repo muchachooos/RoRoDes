9447e5efa9328a00a3833c975e849c0a03000e77
muchachooos <odnorogg@bk.ru>
2023-01-22 15:36:04 +0400

    Create .travis.yml

--- storage/unit.go
+++ storage/unit.go
@@ -5,24 +5,24 @@ import (
 	"github.com/google/uuid"
 )
 
-func (s *Storage) CreateUnitInDB(cardID string) ([]model.Unit, error) {
+func (s *Storage) CreateUnitInDB(cardID, gameID string, fieldNum int) ([]model.Unit, error) {
 	var card []model.Card
 
-	err := s.DB.Select(&card, "SELECT `id`, `name`, `damage`, `speed`, `health` FROM card WHERE `id` = ?", cardID)
+	err := s.DB.Select(&card, "SELECT `card_id`, `name`, `damage`, `speed`, `health`, `picture` FROM card WHERE `card_id` = ?", cardID)
 	if err != nil {
 		return nil, err
 	}
 
 	unitID := uuid.NewString()
 
-	_, err = s.DB.Exec("INSERT INTO unit (`id`, `card_id`,`name`,`damage`,`speed`,`health`) VALUES (?,?,?,?,?,?)", unitID, cardID, card[0].Name, card[0].Damage, card[0].Speed, card[0].Health)
+	_, err = s.DB.Exec("INSERT INTO unit (`unit_id`, `card_id`,`game_id`, `field_num`, `name`, `damage`, `speed`, `health`) VALUES (?,?,?,?,?,?,?,?)", unitID, cardID, gameID, fieldNum, card[0].Name, card[0].Damage, card[0].Speed, card[0].Health)
 	if err != nil {
 		return nil, err
 	}
 
 	var unit []model.Unit
 
-	err = s.DB.Select(&unit, "SELECT `id`, `card_id`, `name`, `damage`, `speed`, `health` FROM unit WHERE `id` = ?", unitID)
+	err = s.DB.Select(&unit, "SELECT `unit_id`, `card_id`, `game_id`, `field_num`, `name`, `damage`, `speed`, `health` FROM unit WHERE `unit_id` = ?", unitID)
 	if err != nil {
 		return nil, err
 	}
