4c56c1118629cdeceda4710e6f1136f8bf5b8a95
Aleksei <113183534+Kovrigin-Al@users.noreply.github.com>
2023-05-18 22:10:14 +0400

    test

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -3,16 +3,18 @@ import counterReducer from "../features/counter/counterSlice";
 import { lobbyAPI } from "../features/lobby/lobbyAPI";
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { authAPI } from "../features/auth/authAPI";
+import { gameAPI } from "../features/game/gameAPI";
 
 
 export const store = configureStore({
   reducer: {
     [lobbyAPI.reducerPath]: lobbyAPI.reducer,
     [authAPI.reducerPath]: authAPI.reducer,
+    [gameAPI.reducerPath]: gameAPI.reducer,
     counter: counterReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(lobbyAPI.middleware).concat(authAPI.middleware)
+    getDefaultMiddleware().concat(lobbyAPI.middleware).concat(authAPI.middleware).concat(lobbyAPI.middleware)
 });
 
 export type AppDispatch = typeof store.dispatch;
