3cc4ef87545188b541acf253514a7f4f097145a3
muchachooos <108815068+muchachooos@users.noreply.github.com>
2023-08-03 12:34:22 +0300

    Modify Get deck API

--- server/storage/deck.go
+++ server/storage/deck.go
@@ -84,14 +84,71 @@ func (s *Storage) DeleteCardFromDB(userLogin, cardID string) error {
 func (s *Storage) GetDeckFromDB(userLogin string) ([]model.CardResponse, error) {
 	var deckID string
 
+	// Получаем ID колоды пользователя
 	err := s.DB.Get(&deckID, "SELECT `id` FROM deck WHERE `user_login` = ?", userLogin)
 	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, model.ErrDeckNotFound
+		}
+		return nil, err
+	}
+
+	var cardsInDeck []model.CardInDeck
+
+	// Получаем все карты в колоде пользователя
+	err = s.DB.Select(&cardsInDeck, "SELECT * FROM card_in_deck WHERE `deck_id` = ?", deckID)
+	if err != nil {
+		return nil, err
+	}
+
+	//Создаём слайс и заполняем его ID карт из колоды
+	cardIDs := make([]string, 0, len(cardsInDeck))
+
+	for i := range cardsInDeck {
+		cardIDs = append(cardIDs, cardsInDeck[i].CardID)
+	}
+
+	query, args, err := sqlx.In("SELECT `card_id`, `name`, `name`, `speed`, `health` FROM card WHERE `card_id` IN (?)", cardIDs)
+	if err != nil {
+		return nil, err
+	}
+
+	var cards []model.CardResponse
+
+	//Находим все карты с нужными ID
+	err = s.DB.Select(&cards, query, args...)
+	if err != nil {
+		return nil, err
+	}
+
+	return cards, nil
+}
+
+/*
+var cardIDs []model.Card
+
+err = s.DB.Select(&cardIDs, "SELECT * FROM card WHERE `card_id` IN (SELECT card_id FROM card_in_deck WHERE `deck_id` = ?)", deckID)
+if err != nil {
+return nil, err
+}
+*/
+
+/*
+func (s *Storage) GetDeckFromDB(userLogin string) ([]model.CardResponse, error) {
+	var deckID string
+
+	// Получаем ID колоды пользователя
+	err := s.DB.Get(&deckID, "SELECT `id` FROM deck WHERE `user_login` = ?", userLogin)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, model.ErrDeckNotFound
+		}
 		return nil, err
 	}
 
 	var cardIDs []string
 
-	err = s.DB.Select(&cardIDs, "SELECT `card_id` FROM card_in_deck WHERE `deck_id` = ?", deckID)
+	err = s.DB.Select(&cardIDs, "SELECT * FROM card_in_deck WHERE `deck_id` = ?", deckID)
 	if err != nil {
 		return nil, err
 	}
@@ -110,3 +167,4 @@ func (s *Storage) GetDeckFromDB(userLogin string) ([]model.CardResponse, error) 
 
 	return cardInDeck, nil
 }
+*/
